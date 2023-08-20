6736ae6a8cab79f90cca36374ba6be64cbf69233
Mateusz <mateuszgrzegorzewski@gazeta.pl>
2023-08-20 11:40:37 +0200

    Improving validation which unable adding expenses with future date

--- Finances.Application/Expense/Commands/CreateExpense/CreateExpenseCommandValidator.cs
+++ Finances.Application/Expense/Commands/CreateExpense/CreateExpenseCommandValidator.cs
@@ -20,6 +20,9 @@ namespace Finances.Application.Expense.Commands.CreateExpense
             RuleFor(c => c.Category)
                 .NotEmpty().WithMessage("Please choose Category.");
 
+            RuleFor(c => c.CreatedAt)
+                .LessThanOrEqualTo(DateTime.Now).WithMessage("Expense cannot be added with future date");
+
             RuleFor(c => c.Description)
                 .MaximumLength(256).WithMessage("Description should have maximum of 256 characters.");
         }
