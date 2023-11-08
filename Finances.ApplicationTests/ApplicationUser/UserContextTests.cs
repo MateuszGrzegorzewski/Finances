a8d2a0d8d735fe125f62e5aef9f5fb9cb3485668
Mateusz <mateuszgrzegorzewski@gazeta.pl>
2023-11-08 19:55:02 +0100

    Creating unit tests for UserContext in Application section

---
+++ Finances.ApplicationTests/ApplicationUser/UserContextTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using Xunit;
+
+namespace Finances.Application.ApplicationUser.Tests
+{
+    public class UserContextTests
+    {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            // arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Email, "test@example.com"),
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            //act
+            var currentUser = userContext.GetCurrentUser();
+
+            //assert
+            currentUser.Should().NotBeNull();
+            currentUser.Id.Should().Be("1");
+            currentUser.Email.Should().Be("test@example.com");
+        }
+    }
+}
