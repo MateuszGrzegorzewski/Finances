98c3ccab5d567b77c3eb8b5a13cce1ff5778db11
Mateusz <mateuszgrzegorzewski@gazeta.pl>
2023-11-08 19:10:54 +0100

    Creating unit tests for Domain part of application

---
+++ Finances.DomainTests/Entities/CategoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Finances.Domain.Entities.Tests
+{
+    public class CategoryTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            var category = new Category();
+            category.Name = "Test Category";
+
+            category.EncodeName();
+
+            category.EncodedName.Should().Be("test-category");
+        }
+
+        [Fact()]
+        public void EncodeName_shouldThrrowException_WhenNameIsNull()
+        {
+            var category = new Category();
+
+            Action action = () => category.EncodeName();
+
+            action.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
